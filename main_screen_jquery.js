6c3f6c8e5d87282db810dc69d39868644b20528d
Yangnyeon <tmdgurdla324@naver.com>
2022-07-19 01:37:01 +0900

    07/19

---
+++ main_screen_jquery.js
@@ -0,0 +1,12 @@
+$(function() {
+  $('#menu1, #menu2, #menu3, #menu4').mouseover(function(){
+    $(this).css('color','#fff');
+  });
+});
+
+$(function() {
+  $('#menu1, #menu2, #menu3, #menu4').mouseout(function(){
+    $(this).css('color','#000');
+  });
+});
+
